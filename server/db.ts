04693d5e73fe31bdd9435814b1096338bee5fbca
Sudip Biswas <sudipbiswas2142@gmail.com>
2024-10-29 17:12:33 +0530

    added driver adapters for prisma

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,16 @@
-import "server-only";
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from "@neondatabase/serverless";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const db = globalThis.prisma ?? new PrismaClient();
+// Edge runtime support
+const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaNeon(neon);
+const db = globalThis.prisma ?? new PrismaClient({ adapter });
+
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
 export default db;
